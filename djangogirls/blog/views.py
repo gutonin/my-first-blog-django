f27d132361c18a99582c4b1b1c102ffc02b83d23
gustavo tonin <gutonin@hotmail.com>
2020-12-02 12:04:30 -0300

    Addicionado create/edit posts blog no site

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.shortcuts import render
+from .forms import PostForm
+
 
 def post_list(request):
-    return render (request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
